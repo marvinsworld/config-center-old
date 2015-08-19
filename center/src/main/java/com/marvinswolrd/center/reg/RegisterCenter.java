cad4a85a345ebcc377de8f434ba07ba27e54f3d6
marvinsworld <marvinsworld@126.com>
2015-08-20 06:53:40 +0800

    '开始注册中心'

---
+++ center/src/main/java/com/marvinswolrd/center/reg/RegisterCenter.java
@@ -0,0 +1,23 @@
+package com.marvinswolrd.center.reg;
+/**
+ * Description:
+ *
+ * @author Marvinsworld
+ * @since 2015/8/20 6:48
+ */
+public class RegisterCenter {
+
+    /**
+     * 注册节点
+     */
+    public void regNode(){
+
+    }
+
+    /**
+     * 创建连接
+     */
+    public void createConnection(){
+
+    }
+}
