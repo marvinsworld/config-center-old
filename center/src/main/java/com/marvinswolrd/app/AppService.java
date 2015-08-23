0b84ab466103e4868501f267a66972841a3c8ddb
marvinsworld <marvinsworld@126.com>
2015-08-23 08:24:52 +0800

    'spring'

---
+++ center/src/main/java/com/marvinswolrd/app/AppService.java
@@ -0,0 +1,12 @@
+package com.marvinswolrd.app;
+
+/**
+ * Description:
+ *
+ * @author Marvinsworld
+ * @since 2015/8/22 22:44
+ */
+public interface AppService {
+
+    public void getParam();
+}
--- center/src/main/java/com/marvinswolrd/center/reg/RegisterCenter.java
+++ center/src/main/java/com/marvinswolrd/center/reg/RegisterCenter.java
@@ -1,4 +1,11 @@
 package com.marvinswolrd.center.reg;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+
 /**
  * Description:
  *
@@ -6,6 +13,17 @@ package com.marvinswolrd.center.reg;
  * @since 2015/8/20 6:48
  */
 public class RegisterCenter {
+
+
+    public CuratorFramework createClient() {
+        RetryPolicy policy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("192.168.8.3:2181", policy);
+        return client;
+    }
+
+    public void init(){
+        System.out.println("-------------");
+    }
 
     /**
      * 注册节点
@@ -19,5 +37,23 @@ public class RegisterCenter {
      */
     public void createConnection(){
 
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        RegisterCenter configCenter = new RegisterCenter();
+
+        CuratorFramework client = configCenter.createClient();
+        client.start();
+
+        client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath("/create", "222".getBytes());
+
+//        client.newNamespaceAwareEnsurePath("/create");
+//
+//
+//        client.setData().forPath("/create", "222".getBytes());
+        System.out.println(new String(client.getData().forPath("/create")));
+        System.out.println("111");
     }
 }
