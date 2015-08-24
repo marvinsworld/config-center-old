494f1ce1ee2758148752c74dc9ee7073345cd19a
zhiqiang.ge <zhiqiang.ge@dmall.com>
2015-08-24 18:12:33 +0800

    '修改注解'

--- center/src/main/java/com/marvinswolrd/annotation/DConfigBeanPostProcessor.java
+++ center/src/main/java/com/marvinswolrd/annotation/DConfigBeanPostProcessor.java
@@ -1,6 +1,11 @@
 package com.marvinswolrd.annotation;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.Date;
 
 /**
  * Description:.
@@ -11,26 +16,26 @@ import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostP
  */
 public class DConfigBeanPostProcessor extends AutowiredAnnotationBeanPostProcessor {
 
-//    public Object postProcessBeforeInitialization(final Object bean, String beanName)
-//            throws BeansException {
-//        ReflectionUtils.doWithFields(bean.getClass(),
-//                new ReflectionUtils.FieldCallback() {
-//                    public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
-//                        field.set(bean, new Date());
-//                    }
-//                }, new ReflectionUtils.FieldFilter() {
-//                    public boolean matches(Field field) {
-//                        return field.getAnnotation(DConfig.class) != null;
-//                        //return true;
-//                        //return field.getType() == Date.class && field.getAnnotation(TimeStamp.class) != null;
-//                    }
-//                });
-//
-//        return bean;
-//    }
-//
-//    public Object postProcessAfterInitialization(Object bean, String beanName)
-//            throws BeansException {
-//        return bean;
-//    }
+    public Object postProcessBeforeInitialization(final Object bean, String beanName)
+            throws BeansException {
+        ReflectionUtils.doWithFields(bean.getClass(),
+                new ReflectionUtils.FieldCallback() {
+                    public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
+                        field.set(bean, new Date());
+                    }
+                }, new ReflectionUtils.FieldFilter() {
+                    public boolean matches(Field field) {
+                        return field.getAnnotation(DConfig.class) != null;
+                        //return true;
+                        //return field.getType() == Date.class && field.getAnnotation(TimeStamp.class) != null;
+                    }
+                });
+
+        return bean;
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+        return bean;
+    }
 }
