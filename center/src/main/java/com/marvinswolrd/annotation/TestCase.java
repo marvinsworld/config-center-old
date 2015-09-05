4b52656adbbc93eed329975089921205ae8b420d
marvinsworld <marvinsworld@126.com>
2015-09-05 23:48:22 +0800

    'init'

--- center/src/main/java/com/marvinswolrd/annotation/TestCase.java
+++ center/src/main/java/com/marvinswolrd/annotation/TestCase.java
@@ -23,6 +23,9 @@ public class TestCase {
     @DConfig("bbb")
     private String db;
 
+    @DConfig("${bbb}")
+    private String db1;
+
     @Value("${bbb}")
     private String db2;
 
@@ -34,9 +37,23 @@ public class TestCase {
         return db;
     }
 
+    public String getDb1() {
+        return db1;
+    }
 
+    public void setDb1(String db1) {
+        this.db1 = db1;
+    }
 
-//    public String getDb2() {
+    public String getDb2() {
+        return db2;
+    }
+
+    public void setDb2(String db2) {
+        this.db2 = db2;
+    }
+
+    //    public String getDb2() {
 //        return db2;
 //    }
 //
